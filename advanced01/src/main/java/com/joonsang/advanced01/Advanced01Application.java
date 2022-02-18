38b30e49517c3c5350dbe25d6a3a07c3496fcc1e
junsang <gufrus@naver.com>
2022-02-18 16:02:27 +0900

    [2022.02.18] 스프링 핵심 원리 - 고급편 - 프로젝트 생성

---
+++ advanced01/src/main/java/com/joonsang/advanced01/Advanced01Application.java
@@ -0,0 +1,13 @@
+package com.joonsang.advanced01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Advanced01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Advanced01Application.class, args);
+	}
+
+}
